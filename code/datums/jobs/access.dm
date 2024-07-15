tgstation/TerraGov-Marine-Corps

Transport Officer (#15004)

code/datums/jobs/access.dm
@@ -39,7 +39,7 @@
 		if(0)
 			return ALL_ACCESS
 		if(1)
-			return list(ACCESS_MARINE_CAPTAIN, ACCESS_MARINE_COMMANDER, ACCESS_MARINE_LOGISTICS, ACCESS_MARINE_DROPSHIP, ACCESS_MARINE_PILOT, ACCESS_MARINE_MECH, ACCESS_MARINE_BRIDGE)//command
+			return list(ACCESS_MARINE_CAPTAIN, ACCESS_MARINE_COMMANDER, ACCESS_MARINE_LOGISTICS, ACCESS_MARINE_DROPSHIP, ACCESS_MARINE_TADPOLE, ACCESS_MARINE_PILOT, ACCESS_MARINE_MECH, ACCESS_MARINE_BRIDGE)//command
 		if(2)
 			return list(ACCESS_MARINE_CE, ACCESS_MARINE_ENGINEERING, ACCESS_MARINE_REMOTEBUILD)//engineering and maintenance
 		if(3)
@@ -132,6 +132,8 @@
 			return "Requisitions"
 		if(ACCESS_MARINE_DROPSHIP)
 			return "Dropship Piloting"
+		if(ACCESS_MARINE_TADPOLE)
+			return "Tadpole Piloting"
 		if(ACCESS_MARINE_PILOT)
 			return "Pilot Gear"
 		if(ACCESS_MARINE_MECH)

PMC-Unga-Marines/Unga-Marines

code/datums/jobs/access.dm
@@ -39,7 +39,7 @@
 		if(0)
 			return ALL_ACCESS
 		if(1)
-			return list(ACCESS_MARINE_CAPTAIN, ACCESS_MARINE_COMMANDER, ACCESS_MARINE_LOGISTICS, ACCESS_MARINE_DROPSHIP, ACCESS_MARINE_TADPOLE, ACCESS_MARINE_PILOT, ACCESS_MARINE_MECH, ACCESS_MARINE_BRIDGE, ACCESS_MARINE_ARMORED)//command
+			return list(ACCESS_MARINE_CAPTAIN, ACCESS_MARINE_COMMANDER, ACCESS_MARINE_LOGISTICS, ACCESS_MARINE_DROPSHIP, ACCESS_MARINE_PILOT, ACCESS_MARINE_MECH, ACCESS_MARINE_BRIDGE, ACCESS_MARINE_ARMORED)//command
 		if(2)
 			return list(ACCESS_MARINE_CE, ACCESS_MARINE_ENGINEERING, ACCESS_MARINE_REMOTEBUILD)//engineering and maintenance
 		if(3)
@@ -132,6 +132,8 @@
 			return "Requisitions"
 		if(ACCESS_MARINE_DROPSHIP)
 			return "Dropship Piloting"
+		if(ACCESS_MARINE_TADPOLE)
+			return "Tadpole Piloting"
 		if(ACCESS_MARINE_PILOT)
 			return "Pilot Gear"
 		if(ACCESS_MARINE_MECH)