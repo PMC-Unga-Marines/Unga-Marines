tgstation/TerraGov-Marine-Corps

Jungle walls spawn weedable grass when hit with a devastating explosion (#15390)
Co-authored-by: Barnet <you@example.com>

code/game/turfs/closed.dm
@@ -191,7 +191,7 @@
 /turf/closed/gm/ex_act(severity)
 	switch(severity)
 		if(EXPLODE_DEVASTATE)
-			ChangeTurf(/turf/open/ground/grass)
+			ChangeTurf(/turf/open/ground/grass/weedable)
 
 /turf/closed/gm/dense
 	name = "dense jungle wall"

PMC-Unga-Marines/Unga-Marines

code/game/turfs/closed.dm
@@ -229,7 +229,7 @@
 
 /turf/closed/gm/ex_act(severity)
 	if(severity >= EXPLODE_DEVASTATE)
-		change_turf(/turf/open/ground/grass)
+		change_turf(/turf/open/ground/grass/weedable)
 
 /turf/closed/gm/dense
 	name = "dense jungle wall"