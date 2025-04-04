tgstation/TerraGov-Marine-Corps

Object damage fix (#15448)

code/modules/vehicles/_hitbox.dm
@@ -169,7 +169,7 @@
 	SEND_SIGNAL(src, COMSIG_ATOM_BULLET_ACT, proj)
 	return root.bullet_act(proj)
 
-/obj/hitbox/take_damage(damage_amount, damage_type = BRUTE, armor_type = MELEE, effects = TRUE, attack_dir, armour_penetration = 0, mob/living/blame_mob)
+/obj/hitbox/take_damage(damage_amount, damage_type = BRUTE, armor_type = null, effects = TRUE, attack_dir, armour_penetration = 0, mob/living/blame_mob)
 	return root.take_damage(arglist(args))
 
 /obj/hitbox/ex_act(severity)

PMC-Unga-Marines/Unga-Marines

code/modules/vehicles/_hitbox.dm
@@ -209,7 +209,7 @@
 	SEND_SIGNAL(src, COMSIG_ATOM_BULLET_ACT, proj)
 	return root.bullet_act(proj)
 
-/obj/hitbox/take_damage(damage_amount, damage_type = BRUTE, damage_flag = "", effects = TRUE, attack_dir, armour_penetration = 0, mob/living/blame_mob)
+/obj/hitbox/take_damage(damage_amount, damage_type = BRUTE, damage_flag = null, effects = TRUE, attack_dir, armour_penetration = 0, mob/living/blame_mob)
 	return root.take_damage(arglist(args))
 
 /obj/hitbox/ex_act(severity)