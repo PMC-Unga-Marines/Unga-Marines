tgstation/TerraGov-Marine-Corps

Jump mod implant (#15678)

code/game/objects/items/implants/implanter.dm
@@ -92,3 +92,7 @@
 	if(!.)
 		return
 	qdel(src)
+
+/obj/item/implanter/jump_mod
+	name = "fortified ankles implant"
+	imp = /obj/item/implant/jump_mod

PMC-Unga-Marines/Unga-Marines

code/game/objects/items/implants/implanter.dm
@@ -99,3 +99,7 @@
 	if(!.)
 		return
 	qdel(src)
+
+/obj/item/implanter/jump_mod
+	name = "fortified ankles implant"
+	internal_implant = /obj/item/implant/jump_mod