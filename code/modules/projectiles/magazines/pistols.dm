tgstation/TerraGov-Marine-Corps

HvH faction remastering and adding: Vyacheslav Security Detail pt.1 (#15844)

code/modules/projectiles/magazines/pistols.dm
@@ -272,3 +272,23 @@
 	caliber = CALIBER_ALIEN
 	icon_state = "knife"
 	max_rounds = 1
+
+//XM104 cylinder placed in pistols
+/obj/item/ammo_magazine/pistol/xmdivider
+	name = "\improper XM104 cylinder (.357)"
+	desc = "XM104 cylinder loaded with custom .357 incendiary rounds."
+	default_ammo = /datum/ammo/bullet/revolver/heavy/incen
+	max_rounds = 6
+	caliber = CALIBER_357
+	icon_state = "xm104"
+	icon_state_mini = "mag_revolver_red"
+
+//c96
+
+/obj/item/ammo_magazine/pistol/vsd_pistol
+	name = "\improper C96 'riot' magazine"
+	default_ammo = /datum/ammo/bullet/pistol/ap
+	caliber = CALIBER_9X19
+	icon_state = "c96"
+	icon_state_mini = "mag_pistol_normal"
+	max_rounds = 15

PMC-Unga-Marines/Unga-Marines

code/modules/projectiles/magazines/pistols.dm
@@ -288,3 +288,23 @@
 	caliber = CALIBER_ALIEN
 	icon_state = "knife"
 	max_rounds = 1
+
+//XM104 cylinder placed in pistols
+/obj/item/ammo_magazine/pistol/xmdivider
+	name = "\improper XM104 cylinder (.357)"
+	desc = "XM104 cylinder loaded with custom .357 incendiary rounds."
+	default_ammo = /datum/ammo/bullet/revolver/heavy/incendiary
+	max_rounds = 6
+	caliber = CALIBER_357
+	icon_state = "xm104"
+	icon_state_mini = "mag_revolver_red"
+
+//c96
+
+/obj/item/ammo_magazine/pistol/vsd_pistol
+	name = "\improper C96 'riot' magazine"
+	default_ammo = /datum/ammo/bullet/pistol/ap
+	caliber = CALIBER_9X19
+	icon_state = "c96"
+	icon_state_mini = "mag_pistol_normal"
+	max_rounds = 15