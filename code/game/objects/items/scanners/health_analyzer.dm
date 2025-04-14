tgstation/TerraGov-Marine-Corps

Defibrillator refactor, NPCs/disconnected players rework (#15926)

code/game/objects/items/scanners/health_analyzer.dm
@@ -222,7 +222,7 @@ GLOBAL_LIST_INIT(known_implants, subtypesof(/obj/item/implant))
 	else if(issynth(patient))
 		if(patient.health >= patient.get_death_threshold())
 			revivable_patient = TRUE
-	else if(patient.health + patient.getOxyLoss() + (DEFIBRILLATOR_HEALING_TIMES_SKILL(user.skills.getRating(SKILL_MEDICAL))) >= patient.get_death_threshold())
+	else if(patient.health + patient.getOxyLoss() + (DEFIBRILLATOR_HEALING_TIMES_SKILL(user.skills.getRating(SKILL_MEDICAL), DEFIBRILLATOR_BASE_HEALING_VALUE)) >= patient.get_death_threshold())
 		revivable_patient = TRUE
 
 	if(HAS_TRAIT(patient, TRAIT_UNDEFIBBABLE))
@@ -235,7 +235,7 @@ GLOBAL_LIST_INIT(known_implants, subtypesof(/obj/item/implant))
 		data["revivable_string"] = "Ready to [organic_patient ? "defibrillate" : "reboot"]" // Ternary for defibrillate or reboot for some IC flavor
 		data["revivable_boolean"] = TRUE
 	else
-		data["revivable_string"] = "Not ready to [organic_patient ? "defibrillate" : "reboot"] - repair damage above [patient.get_death_threshold() / patient.maxHealth * 100 - (organic_patient ? (DEFIBRILLATOR_HEALING_TIMES_SKILL(user.skills.getRating(SKILL_MEDICAL))) : 0)]%"
+		data["revivable_string"] = "Not ready to [organic_patient ? "defibrillate" : "reboot"] - repair damage above [patient.get_death_threshold() / patient.maxHealth * 100 - (organic_patient ? (DEFIBRILLATOR_HEALING_TIMES_SKILL(user.skills.getRating(SKILL_MEDICAL), DEFIBRILLATOR_BASE_HEALING_VALUE)) : 0)]%"
 		data["revivable_boolean"] = FALSE
 
 	// ADVICE

code/modules/mob/living/carbon/human/human_helpers.dm
@@ -262,3 +262,64 @@
 	SIGNAL_HANDLER
 	UnregisterSignal(SSdcs, COMSIG_GLOB_DEPLOY_TIMELOCK_ENDED)
 	GLOB.squad_selector.interact(src)
+
+/**
+ * Proc to check if a carbon human has the required organs to sustain life.
+ *
+ * Returns false if `has_brain` returns false, this human is missing a heart, or their current heart is broken
+ *
+ * Returns true otherwise
+ */
+/mob/living/carbon/human/proc/has_working_organs()
+	var/datum/internal_organ/heart/heart = internal_organs_by_name["heart"]
+
+	if(species.species_flags & ROBOTIC_LIMBS)
+		return TRUE // combat robots and synthetics don't have any of these for some reason
+	if(!has_brain())
+		return FALSE
+	if(!heart || heart.organ_status == ORGAN_BROKEN)
+		return FALSE
+	return TRUE
+
+/**
+ * Proc that brings a carbon human back to life. Only works if their health is higher than their death threshold and they are dead in the first place.
+ *
+ * Intended to be called by defibrillators or anything that brings a carbon human back to life
+ */
+/mob/living/carbon/human/proc/resuscitate()
+	if(stat != DEAD || health <= get_death_threshold())
+		return
+	set_stat(UNCONSCIOUS)
+	chestburst = CARBON_NO_CHEST_BURST
+	regenerate_icons()
+	reload_fullscreens()
+	handle_regular_hud_updates()
+	updatehealth() //One more time, so it doesn't show the target as dead on HUDs
+	dead_ticks = 0 //We reset the DNR timer
+	REMOVE_TRAIT(src, TRAIT_PSY_DRAINED, TRAIT_PSY_DRAINED)
+
+/**
+ * Proc for checking parameters of a human for defibrillation.
+ *
+ * Checks decapitation, DNR status, organ damage and health status (in that order) for defibrillation.
+ *
+ * See defines in `__DEFINES/defibrillator.dm` for bitflags.
+ *
+ * `additional_health_increase` can be used to add additional health when calculating health for situations like grabbing ghost.
+ */
+/mob/living/carbon/human/proc/check_defib(additional_health_increase = 0)
+
+	var/datum/limb/head/head = get_limb("head")
+	if(head.limb_status & LIMB_DESTROYED)
+		return DEFIB_FAIL_DECAPITATED
+
+	if(HAS_TRAIT(src, TRAIT_UNDEFIBBABLE))
+		return DEFIB_FAIL_BRAINDEAD
+
+	if(!has_working_organs())
+		return DEFIB_FAIL_BAD_ORGANS
+
+	if(health + getOxyLoss() + additional_health_increase <= get_death_threshold())
+		return DEFIB_FAIL_TOO_MUCH_DAMAGE
+
+	return DEFIB_POSSIBLE

PMC-Unga-Marines/Unga-Marines

code/game/objects/items/scanners/health_analyzer.dm
@@ -254,7 +254,7 @@ GLOBAL_LIST_INIT(known_implants, subtypesof(/obj/item/implant))
 	else if(issynth(patient))
 		if(patient.health >= patient.get_death_threshold())
 			revivable_patient = TRUE
-	else if(patient.health + patient.get_oxy_loss() + (DEFIBRILLATOR_HEALING_TIMES_SKILL(user.skills.getRating(SKILL_MEDICAL))) >= patient.get_death_threshold())
+	else if(patient.health + patient.get_oxy_loss() + (DEFIBRILLATOR_HEALING_TIMES_SKILL(user.skills.getRating(SKILL_MEDICAL), DEFIBRILLATOR_BASE_HEALING_VALUE)) >= patient.get_death_threshold())
 		revivable_patient = TRUE
 
 	if(HAS_TRAIT(patient, TRAIT_UNDEFIBBABLE))
@@ -267,7 +267,7 @@ GLOBAL_LIST_INIT(known_implants, subtypesof(/obj/item/implant))
 		data["revivable_string"] = "Ready to [organic_patient ? "defibrillate" : "reboot"]" // Ternary for defibrillate or reboot for some IC flavor
 		data["revivable_boolean"] = TRUE
 	else
-		data["revivable_string"] = "Not ready to [organic_patient ? "defibrillate" : "reboot"] - repair damage above [patient.get_death_threshold() / patient.maxHealth * 100 - (organic_patient ? (DEFIBRILLATOR_HEALING_TIMES_SKILL(user.skills.getRating(SKILL_MEDICAL))) : 0)]%"
+		data["revivable_string"] = "Not ready to [organic_patient ? "defibrillate" : "reboot"] - repair damage above [patient.get_death_threshold() / patient.maxHealth * 100 - (organic_patient ? (DEFIBRILLATOR_HEALING_TIMES_SKILL(user.skills.getRating(SKILL_MEDICAL), DEFIBRILLATOR_BASE_HEALING_VALUE)) : 0)]%"
 		data["revivable_boolean"] = FALSE
 
 	// ADVICE

code/modules/mob/living/carbon/human/human_helpers.dm
@@ -292,3 +292,63 @@
 		if(istype(i, /obj/item/attachable/motiondetector))
 			var/obj/item/attachable/motiondetector/md = i
 			md.clean_operator()
+
+/**
+ * Proc to check if a carbon human has the required organs to sustain life.
+ *
+ * Returns false if `has_brain` returns false, this human is missing a heart, or their current heart is broken
+ *
+ * Returns true otherwise
+ */
+/mob/living/carbon/human/proc/has_working_organs()
+	var/datum/internal_organ/heart/heart = internal_organs_by_name["heart"]
+
+	if(species.species_flags & ROBOTIC_LIMBS)
+		return TRUE // combat robots and synthetics don't have any of these for some reason
+	if(!has_brain())
+		return FALSE
+	if(!heart || heart.organ_status == ORGAN_BROKEN)
+		return FALSE
+	return TRUE
+
+/**
+ * Proc that brings a carbon human back to life. Only works if their health is higher than their death threshold and they are dead in the first place.
+ *
+ * Intended to be called by defibrillators or anything that brings a carbon human back to life
+ */
+/mob/living/carbon/human/proc/resuscitate()
+	if(stat != DEAD || health <= get_death_threshold())
+		return
+	set_stat(UNCONSCIOUS)
+	chestburst = CARBON_NO_CHEST_BURST
+	regenerate_icons()
+	reload_fullscreens()
+	handle_regular_hud_updates()
+	update_health() //One more time, so it doesn't show the target as dead on HUDs
+	dead_ticks = 0 //We reset the DNR timer
+	REMOVE_TRAIT(src, TRAIT_PSY_DRAINED, TRAIT_PSY_DRAINED)
+
+/**
+ * Proc for checking parameters of a human for defibrillation.
+ *
+ * Checks decapitation, DNR status, organ damage and health status (in that order) for defibrillation.
+ *
+ * See defines in `__DEFINES/defibrillator.dm` for bitflags.
+ *
+ * `additional_health_increase` can be used to add additional health when calculating health for situations like grabbing ghost.
+ */
+/mob/living/carbon/human/proc/check_defib(additional_health_increase = 0)
+	var/datum/limb/head/head = get_limb(BODY_ZONE_HEAD)
+	if(head.limb_status & LIMB_DESTROYED)
+		return DEFIB_FAIL_DECAPITATED
+
+	if(HAS_TRAIT(src, TRAIT_UNDEFIBBABLE))
+		return DEFIB_FAIL_BRAINDEAD
+
+	if(!has_working_organs())
+		return DEFIB_FAIL_BAD_ORGANS
+
+	if(health + get_oxy_loss() + additional_health_increase <= get_death_threshold())
+		return DEFIB_FAIL_TOO_MUCH_DAMAGE
+
+	return DEFIB_POSSIBLE