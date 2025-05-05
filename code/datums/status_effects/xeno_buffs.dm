tgstation/TerraGov-Marine-Corps

Makes xenomorph health/sunder healing, evo progress, maturity, and plasma regen scale based on delta_time/second_per_tick (#16575)

code/datums/status_effects/xeno_buffs.dm
@@ -32,7 +32,7 @@
 	owner.balloon_alert(owner, "We are vulnerable again")
 	return ..()
 
-/datum/status_effect/resin_jelly_coating/tick()
+/datum/status_effect/resin_jelly_coating/tick(delta_time)
 	owner.heal_limb_damage(0, 5)
 	return ..()
 
@@ -99,7 +99,7 @@
 	REMOVE_TRAIT(link_target, TRAIT_ESSENCE_LINKED, TRAIT_STATUS_EFFECT(id))
 	return ..()
 
-/datum/status_effect/stacking/essence_link/tick()
+/datum/status_effect/stacking/essence_link/tick(delta_time)
 	var/within_range = get_dist(link_owner, link_target) <= DRONE_ESSENCE_LINK_RANGE
 	if(within_range != was_within_range) // Toggles the link depending on whether the linked xenos are still in range or not.
 		was_within_range = within_range
@@ -223,7 +223,7 @@
 	buff_owner.balloon_alert(buff_owner, "Salve regeneration ended")
 	return ..()
 
-/datum/status_effect/salve_regen/tick()
+/datum/status_effect/salve_regen/tick(delta_time)
 	new /obj/effect/temp_visual/healing(get_turf(buff_owner))
 	var/heal_amount = buff_owner.maxHealth * 0.01
 	buff_owner.adjustFireLoss(-max(0, heal_amount - buff_owner.getBruteLoss()), passive = TRUE)
@@ -293,7 +293,7 @@
 	toggle_buff(FALSE)
 	return ..()
 
-/datum/status_effect/drone_enhancement/tick()
+/datum/status_effect/drone_enhancement/tick(delta_time)
 	var/within_range = get_dist(buffed_xeno, buffing_xeno) <= DRONE_ESSENCE_LINK_RANGE
 	if(within_range != was_within_range)
 		was_within_range = within_range
@@ -556,13 +556,13 @@
 	owner.clear_fullscreen("xeno_feast", 0.7 SECONDS)
 	owner.remove_filter(list("[id]1", "[id]2"))
 
-/datum/status_effect/xeno_feast/tick()
+/datum/status_effect/xeno_feast/tick(delta_time)
 	var/mob/living/carbon/xenomorph/X = owner
 	if(X.plasma_stored < plasma_drain)
 		to_chat(X, span_notice("Our feast has come to an end..."))
 		X.remove_status_effect(STATUS_EFFECT_XENO_FEAST)
 		return
-	var/heal_amount = X.maxHealth*0.08
+	var/heal_amount = X.maxHealth * 0.08
 	HEAL_XENO_DAMAGE(X, heal_amount, FALSE)
 	adjustOverheal(X, heal_amount / 2)
 	X.use_plasma(plasma_drain)
@@ -596,13 +596,13 @@
 	else
 		RegisterSignal(owner, COMSIG_XENOMORPH_PLASMA_REGEN, PROC_REF(plasma_surge_regeneration))
 
-/datum/status_effect/plasma_surge/proc/plasma_surge_regeneration()
+/datum/status_effect/plasma_surge/proc/plasma_surge_regeneration(mob/living/carbon/xenomorph/xeno, plasma_mod, seconds_per_tick)
 	SIGNAL_HANDLER
 
 	var/mob/living/carbon/xenomorph/X = owner
 	if(HAS_TRAIT(X,TRAIT_NOPLASMAREGEN)) //No bonus plasma if you're on a diet
 		return
-	var/bonus_plasma = X.xeno_caste.plasma_gain * bonus_regen * (1 + X.recovery_aura * 0.05) //Recovery aura multiplier; 5% bonus per full level
+	var/bonus_plasma = X.xeno_caste.plasma_gain * bonus_regen * (1 + X.recovery_aura * 0.05) * seconds_per_tick * XENO_PER_SECOND_LIFE_MOD //Recovery aura multiplier; 5% bonus per full level
 	X.gain_plasma(bonus_plasma)
 
 /datum/status_effect/plasma_surge/on_remove()
@@ -661,7 +661,7 @@
 	return ..()
 
 ///Called when the target xeno regains HP via heal_wounds in life.dm
-/datum/status_effect/healing_infusion/proc/healing_infusion_regeneration(mob/living/carbon/xenomorph/patient)
+/datum/status_effect/healing_infusion/proc/healing_infusion_regeneration(mob/living/carbon/xenomorph/patient, heal_data, seconds_per_tick)
 	SIGNAL_HANDLER
 
 	if(!health_ticks_remaining)
@@ -672,7 +672,7 @@
 
 	new /obj/effect/temp_visual/healing(get_turf(patient)) //Cool SFX
 
-	var/total_heal_amount = 6 + (patient.maxHealth * 0.03) //Base amount 6 HP plus 3% of max
+	var/total_heal_amount = 6 + (patient.maxHealth * 0.03) * seconds_per_tick * XENO_PER_SECOND_LIFE_MOD //Base amount 6 HP plus 3% of max
 	if(patient.recovery_aura)
 		total_heal_amount *= (1 + patient.recovery_aura * 0.05) //Recovery aura multiplier; 5% bonus per full level
 
@@ -691,7 +691,7 @@
 
 
 ///Called when the target xeno regains Sunder via heal_wounds in life.dm
-/datum/status_effect/healing_infusion/proc/healing_infusion_sunder_regeneration(mob/living/carbon/xenomorph/patient)
+/datum/status_effect/healing_infusion/proc/healing_infusion_sunder_regeneration(mob/living/carbon/xenomorph/patient, seconds_per_tick)
 	SIGNAL_HANDLER
 
 	if(!sunder_ticks_remaining)
@@ -705,7 +705,7 @@
 
 	new /obj/effect/temp_visual/telekinesis(get_turf(patient)) //Visual confirmation
 
-	patient.adjust_sunder(-1.5 * (1 + patient.recovery_aura * 0.05)) //5% bonus per rank of our recovery aura
+	patient.adjust_sunder(-1.5 * (1 + patient.recovery_aura * 0.05) * seconds_per_tick * XENO_PER_SECOND_LIFE_MOD) //5% bonus per rank of our recovery aura
 
 /atom/movable/screen/alert/status_effect/healing_infusion
 	name = "Healing Infusion"
@@ -780,7 +780,7 @@
 	///weakref to the puppeteer to set strength
 	var/datum/weakref/puppeteer
 
-/datum/status_effect/blessing/tick()
+/datum/status_effect/blessing/tick(delta_time)
 	var/mob/living/carbon/xenomorph/xeno = puppeteer?.resolve()
 	if(!xeno)
 		return

code/modules/mob/living/carbon/xenomorph/life.dm
@@ -3,7 +3,7 @@
 #define XENO_STANDING_HEAL 0.2
 #define XENO_CRIT_DAMAGE 5
 
-/mob/living/carbon/xenomorph/Life()
+/mob/living/carbon/xenomorph/Life(seconds_per_tick, times_fired)
 
 	if(!loc)
 		return
@@ -23,12 +23,12 @@
 		if(xeno_flags & XENO_ZOOMED)
 			if(loc != zoom_turf || lying_angle)
 				zoom_out()
-		update_progression()
-		update_evolving()
+		update_progression(seconds_per_tick)
+		update_evolving(seconds_per_tick)
 
-	handle_living_sunder_updates()
-	handle_living_health_updates()
-	handle_living_plasma_updates()
+	handle_living_sunder_updates(seconds_per_tick)
+	handle_living_health_updates(seconds_per_tick)
+	handle_living_plasma_updates(seconds_per_tick)
 	update_action_button_icons()
 	update_icons(FALSE)
 
@@ -41,9 +41,9 @@
 	if(!(xeno_caste.caste_flags & CASTE_FIRE_IMMUNE) && on_fire) //Sanity check; have to be on fire to actually take the damage.
 		apply_damage((fire_stacks + 3), BURN, blocked = FIRE)
 
-/mob/living/carbon/xenomorph/proc/handle_living_health_updates()
+/mob/living/carbon/xenomorph/proc/handle_living_health_updates(seconds_per_tick)
 	if(health < 0)
-		handle_critical_health_updates()
+		handle_critical_health_updates(seconds_per_tick)
 		return
 	if((health >= maxHealth) || on_fire) //can't regenerate.
 		return
@@ -56,18 +56,18 @@
 		ruler_healing_penalty = 1
 	if(loc_weeds_type || xeno_caste.caste_flags & CASTE_INNATE_HEALING) //We regenerate on weeds or can on our own.
 		if(lying_angle || resting || xeno_caste.caste_flags & CASTE_QUICK_HEAL_STANDING)
-			heal_wounds(XENO_RESTING_HEAL * ruler_healing_penalty * loc_weeds_type ? initial(loc_weeds_type.resting_buff) : 1, TRUE)
+			heal_wounds(XENO_RESTING_HEAL * ruler_healing_penalty * loc_weeds_type ? initial(loc_weeds_type.resting_buff) : 1, TRUE, seconds_per_tick)
 		else
-			heal_wounds(XENO_STANDING_HEAL * ruler_healing_penalty, TRUE) //Major healing nerf if standing.
+			heal_wounds(XENO_STANDING_HEAL * ruler_healing_penalty, TRUE, seconds_per_tick) //Major healing nerf if standing.
 	updatehealth()
 
 ///Handles sunder modification/recovery during life.dm for xenos
-/mob/living/carbon/xenomorph/proc/handle_living_sunder_updates()
+/mob/living/carbon/xenomorph/proc/handle_living_sunder_updates(seconds_per_tick)
 
 	if(!sunder || on_fire) //No sunder, no problem; or we're on fire and can't regenerate.
 		return
 
-	var/sunder_recov = xeno_caste.sunder_recover * -0.5 //Baseline
+	var/sunder_recov = xeno_caste.sunder_recover * -0.5 * seconds_per_tick * XENO_PER_SECOND_LIFE_MOD //Baseline
 
 	if(resting) //Resting doubles sunder recovery
 		sunder_recov *= 2
@@ -78,17 +78,17 @@
 	if(recovery_aura)
 		sunder_recov *= 1 + recovery_aura * 0.1 //10% bonus per rank of recovery aura
 
-	SEND_SIGNAL(src, COMSIG_XENOMORPH_SUNDER_REGEN, src)
+	SEND_SIGNAL(src, COMSIG_XENOMORPH_SUNDER_REGEN, seconds_per_tick)
 
 	adjust_sunder(sunder_recov)
 
-/mob/living/carbon/xenomorph/proc/handle_critical_health_updates()
+/mob/living/carbon/xenomorph/proc/handle_critical_health_updates(seconds_per_tick)
 	if(loc_weeds_type)
-		heal_wounds(XENO_RESTING_HEAL)
+		heal_wounds(XENO_RESTING_HEAL, seconds_per_tick)
 	else if(!endure) //If we're not Enduring we bleed out
-		adjustBruteLoss(XENO_CRIT_DAMAGE)
+		adjustBruteLoss(XENO_CRIT_DAMAGE * seconds_per_tick * XENO_PER_SECOND_LIFE_MOD)
 
-/mob/living/carbon/xenomorph/proc/heal_wounds(multiplier = XENO_RESTING_HEAL, scaling = FALSE)
+/mob/living/carbon/xenomorph/proc/heal_wounds(multiplier = XENO_RESTING_HEAL, scaling = FALSE, seconds_per_tick)
 	var/amount = 1 + (maxHealth * 0.0375) // 1 damage + 3.75% max health, with scaling power.
 	if(recovery_aura)
 		amount += recovery_aura * maxHealth * 0.01 // +1% max health per recovery level, up to +5%
@@ -96,19 +96,19 @@
 		if(recovery_aura)
 			regen_power = clamp(regen_power + xeno_caste.regen_ramp_amount*30,0,1) //Ignores the cooldown, and gives a 50% boost.
 		else if(regen_power < 0) // We're not supposed to regenerate yet. Start a countdown for regeneration.
-			regen_power += 2 SECONDS //Life ticks are 2 seconds.
+			regen_power += seconds_per_tick
 			return
 		else
 			regen_power = min(regen_power + xeno_caste.regen_ramp_amount*20,1)
 		amount *= regen_power
-	amount *= multiplier * GLOB.xeno_stat_multiplicator_buff
+	amount *= multiplier * GLOB.xeno_stat_multiplicator_buff * seconds_per_tick * XENO_PER_SECOND_LIFE_MOD
 
 	var/list/heal_data = list(amount)
-	SEND_SIGNAL(src, COMSIG_XENOMORPH_HEALTH_REGEN, heal_data)
+	SEND_SIGNAL(src, COMSIG_XENOMORPH_HEALTH_REGEN, heal_data, seconds_per_tick)
 	HEAL_XENO_DAMAGE(src, heal_data[1], TRUE)
 	return heal_data[1]
 
-/mob/living/carbon/xenomorph/proc/handle_living_plasma_updates()
+/mob/living/carbon/xenomorph/proc/handle_living_plasma_updates(seconds_per_tick)
 	var/turf/T = loc
 	if(!istype(T)) //This means plasma doesn't update while you're in things like a vent, but since you don't have weeds in a vent or can actually take advantage of pheros, this is fine
 		return
@@ -122,7 +122,7 @@
 			QDEL_NULL(current_aura)
 			src.balloon_alert(src, "Stop emitting, no plasma")
 		else
-			use_plasma(pheromone_cost, FALSE)
+			use_plasma(pheromone_cost * seconds_per_tick * XENO_PER_SECOND_LIFE_MOD, FALSE)
 
 	if(HAS_TRAIT(src, TRAIT_NOPLASMAREGEN) || !loc_weeds_type && !(xeno_caste.caste_flags & CASTE_INNATE_PLASMA_REGEN))
 		if(current_aura) //we only need to update if we actually used plasma from pheros
@@ -136,9 +136,11 @@
 
 	plasma_gain *= loc_weeds_type ? initial(loc_weeds_type.resting_buff) : 1
 
+	plasma_gain *= seconds_per_tick * XENO_PER_SECOND_LIFE_MOD
+
 	var/list/plasma_mod = list(plasma_gain)
 
-	SEND_SIGNAL(src, COMSIG_XENOMORPH_PLASMA_REGEN, plasma_mod)
+	SEND_SIGNAL(src, COMSIG_XENOMORPH_PLASMA_REGEN, plasma_mod, seconds_per_tick)
 
 	plasma_mod[1] = clamp(plasma_mod[1], 0, xeno_caste.plasma_max * xeno_caste.plasma_regen_limit - plasma_stored)
 

PMC-Unga-Marines/Unga-Marines

code/datums/status_effects/xeno_buffs.dm
@@ -32,7 +32,7 @@
 	owner.balloon_alert(owner, "We are vulnerable again")
 	return ..()
 
-/datum/status_effect/resin_jelly_coating/tick()
+/datum/status_effect/resin_jelly_coating/tick(delta_time)
 	owner.heal_limb_damage(0, 5)
 	return ..()
 
@@ -99,7 +99,7 @@
 	REMOVE_TRAIT(link_target, TRAIT_ESSENCE_LINKED, TRAIT_STATUS_EFFECT(id))
 	return ..()
 
-/datum/status_effect/stacking/essence_link/tick()
+/datum/status_effect/stacking/essence_link/tick(delta_time)
 	var/within_range = get_dist(link_owner, link_target) <= DRONE_ESSENCE_LINK_RANGE
 	if(within_range != was_within_range) // Toggles the link depending on whether the linked xenos are still in range or not.
 		was_within_range = within_range
@@ -223,7 +223,7 @@
 	buff_owner.balloon_alert(buff_owner, "Salve regeneration ended")
 	return ..()
 
-/datum/status_effect/salve_regen/tick()
+/datum/status_effect/salve_regen/tick(delta_time)
 	new /obj/effect/temp_visual/healing(get_turf(buff_owner))
 	var/heal_amount = buff_owner.maxHealth * 0.01
 	buff_owner.adjust_fire_loss(-max(0, heal_amount - buff_owner.get_brute_loss()), passive = TRUE)
@@ -293,7 +293,7 @@
 	toggle_buff(FALSE)
 	return ..()
 
-/datum/status_effect/drone_enhancement/tick()
+/datum/status_effect/drone_enhancement/tick(delta_time)
 	var/within_range = get_dist(buffed_xeno, buffing_xeno) <= DRONE_ESSENCE_LINK_RANGE
 	if(within_range != was_within_range)
 		was_within_range = within_range
@@ -362,7 +362,7 @@
 	owner.remove_movespeed_modifier(MOVESPEED_ID_GORGER_REJUVENATE)
 	owner.remove_filter("[id]m")
 
-/datum/status_effect/xeno_rejuvenate/tick()
+/datum/status_effect/xeno_rejuvenate/tick(delta_time)
 	var/mob/living/carbon/xenomorph/owner_xeno = owner
 	if(owner_xeno.plasma_stored < GORGER_REJUVENATE_COST)
 		to_chat(owner_xeno, span_notice("Not enough substance to sustain ourselves..."))
@@ -630,7 +630,7 @@
 	owner.clear_fullscreen("xeno_feast", 0.7 SECONDS)
 	owner.remove_filter(list("[id]1", "[id]2"))
 
-/datum/status_effect/xeno_feast/tick()
+/datum/status_effect/xeno_feast/tick(delta_time)
 	. = ..()
 	var/mob/living/carbon/xenomorph/xeno_owner = owner
 
@@ -710,13 +710,13 @@
 	else
 		RegisterSignal(owner, COMSIG_XENOMORPH_PLASMA_REGEN, PROC_REF(plasma_surge_regeneration))
 
-/datum/status_effect/plasma_surge/proc/plasma_surge_regeneration()
+/datum/status_effect/plasma_surge/proc/plasma_surge_regeneration(mob/living/carbon/xenomorph/xeno, plasma_mod, seconds_per_tick)
 	SIGNAL_HANDLER
 
 	var/mob/living/carbon/xenomorph/X = owner
 	if(HAS_TRAIT(X,TRAIT_NOPLASMAREGEN)) //No bonus plasma if you're on a diet
 		return
-	var/bonus_plasma = X.xeno_caste.plasma_gain * bonus_regen * (1 + X.recovery_aura * 0.05) //Recovery aura multiplier; 5% bonus per full level
+	var/bonus_plasma = X.xeno_caste.plasma_gain * bonus_regen * (1 + X.recovery_aura * 0.05) * seconds_per_tick * XENO_PER_SECOND_LIFE_MOD //Recovery aura multiplier; 5% bonus per full level
 	X.gain_plasma(bonus_plasma)
 
 /datum/status_effect/plasma_surge/on_remove()
@@ -775,7 +775,7 @@
 	return ..()
 
 ///Called when the target xeno regains HP via heal_wounds in life.dm
-/datum/status_effect/healing_infusion/proc/healing_infusion_regeneration(mob/living/carbon/xenomorph/patient)
+/datum/status_effect/healing_infusion/proc/healing_infusion_regeneration(mob/living/carbon/xenomorph/patient, heal_data, seconds_per_tick)
 	SIGNAL_HANDLER
 
 	if(!health_ticks_remaining)
@@ -786,7 +786,7 @@
 
 	new /obj/effect/temp_visual/healing(get_turf(patient)) //Cool SFX
 
-	var/total_heal_amount = 6 + (patient.maxHealth * 0.03) //Base amount 6 HP plus 3% of max
+	var/total_heal_amount = 6 + (patient.maxHealth * 0.03) * seconds_per_tick * XENO_PER_SECOND_LIFE_MOD //Base amount 6 HP plus 3% of max
 	if(patient.recovery_aura)
 		total_heal_amount *= (1 + patient.recovery_aura * 0.05) //Recovery aura multiplier; 5% bonus per full level
 
@@ -805,7 +805,7 @@
 
 
 ///Called when the target xeno regains Sunder via heal_wounds in life.dm
-/datum/status_effect/healing_infusion/proc/healing_infusion_sunder_regeneration(mob/living/carbon/xenomorph/patient)
+/datum/status_effect/healing_infusion/proc/healing_infusion_sunder_regeneration(mob/living/carbon/xenomorph/patient, seconds_per_tick)
 	SIGNAL_HANDLER
 
 	if(!sunder_ticks_remaining)
@@ -819,7 +819,7 @@
 
 	new /obj/effect/temp_visual/telekinesis(get_turf(patient)) //Visual confirmation
 
-	patient.adjust_sunder(-1.5 * (1 + patient.recovery_aura * 0.05)) //5% bonus per rank of our recovery aura
+	patient.adjust_sunder(-1.5 * (1 + patient.recovery_aura * 0.05) * seconds_per_tick * XENO_PER_SECOND_LIFE_MOD) //5% bonus per rank of our recovery aura
 
 /atom/movable/screen/alert/status_effect/healing_infusion
 	name = "Healing Infusion"
@@ -1021,7 +1021,7 @@
 	chamber_scaling = length(buff_owner.hive.shell_chambers)
 	return TRUE
 
-/datum/status_effect/upgrade_regeneration/tick()
+/datum/status_effect/upgrade_regeneration/tick(delta_time)
 	chamber_scaling = length(buff_owner.hive.shell_chambers)
 	if(chamber_scaling > 0)
 		var/amount = buff_owner.maxHealth * regen_buff_per_chamber * chamber_scaling * (1 + buff_owner.recovery_aura * 0.05)
@@ -1139,7 +1139,7 @@
 	chamber_scaling = length(buff_owner.hive.spur_chambers)
 	return TRUE
 
-/datum/status_effect/upgrade_adrenaline/tick()
+/datum/status_effect/upgrade_adrenaline/tick(delta_time)
 	if(HAS_TRAIT(buff_owner, TRAIT_NOPLASMAREGEN))
 		return
 	chamber_scaling = length(buff_owner.hive.spur_chambers)

code/modules/mob/living/carbon/xenomorph/life.dm
@@ -3,7 +3,7 @@
 #define XENO_STANDING_HEAL 0.2
 #define XENO_CRIT_DAMAGE 5
 
-/mob/living/carbon/xenomorph/Life()
+/mob/living/carbon/xenomorph/Life(seconds_per_tick, times_fired)
 
 	if(!loc)
 		return
@@ -26,12 +26,12 @@
 					zoom_out()
 			if(lying_angle)
 				zoom_out()
-		update_progression()
-		update_evolving()
+		update_progression(seconds_per_tick)
+		update_evolving(seconds_per_tick)
 
-	handle_living_sunder_updates()
-	handle_living_health_updates()
-	handle_living_plasma_updates()
+	handle_living_sunder_updates(seconds_per_tick)
+	handle_living_health_updates(seconds_per_tick)
+	handle_living_plasma_updates(seconds_per_tick)
 	handle_interference()
 	update_action_button_icons()
 	update_icons(FALSE)
@@ -45,9 +45,9 @@
 	if(!(xeno_caste.caste_flags & CASTE_FIRE_IMMUNE) && on_fire) //Sanity check; have to be on fire to actually take the damage.
 		apply_damage((fire_stacks + 3), BURN, blocked = FIRE)
 
-/mob/living/carbon/xenomorph/proc/handle_living_health_updates()
+/mob/living/carbon/xenomorph/proc/handle_living_health_updates(seconds_per_tick)
 	if(health < 0)
-		handle_critical_health_updates()
+		handle_critical_health_updates(seconds_per_tick)
 		return
 	if(health >= maxHealth || on_fire) //can't regenerate.
 		return
@@ -60,18 +60,18 @@
 		ruler_healing_penalty = 1
 	if(loc_weeds_type || xeno_caste.caste_flags & CASTE_INNATE_HEALING) //We regenerate on weeds or can on our own.
 		if(lying_angle || resting || xeno_caste.caste_flags & CASTE_QUICK_HEAL_STANDING)
-			heal_wounds(XENO_RESTING_HEAL * ruler_healing_penalty * loc_weeds_type ? initial(loc_weeds_type.resting_buff) : 1, TRUE)
+			heal_wounds(XENO_RESTING_HEAL * ruler_healing_penalty * loc_weeds_type ? initial(loc_weeds_type.resting_buff) : 1, TRUE, seconds_per_tick)
 		else
-			heal_wounds(XENO_STANDING_HEAL * ruler_healing_penalty, TRUE) //Major healing nerf if standing.
+			heal_wounds(XENO_STANDING_HEAL * ruler_healing_penalty, TRUE, seconds_per_tick) //Major healing nerf if standing.
 	update_health()
 
 ///Handles sunder modification/recovery during life.dm for xenos
-/mob/living/carbon/xenomorph/proc/handle_living_sunder_updates()
+/mob/living/carbon/xenomorph/proc/handle_living_sunder_updates(seconds_per_tick)
 
 	if(!sunder || on_fire) //No sunder, no problem; or we're on fire and can't regenerate.
 		return
 
-	var/sunder_recov = xeno_caste.sunder_recover * -0.5 //Baseline
+	var/sunder_recov = xeno_caste.sunder_recover * -0.5 * seconds_per_tick * XENO_PER_SECOND_LIFE_MOD //Baseline
 
 	if(resting) //Resting doubles sunder recovery
 		sunder_recov *= 2
@@ -82,17 +82,17 @@
 	if(recovery_aura)
 		sunder_recov *= 1 + recovery_aura * 0.1 //10% bonus per rank of recovery aura
 
-	SEND_SIGNAL(src, COMSIG_XENOMORPH_SUNDER_REGEN, src)
+	SEND_SIGNAL(src, COMSIG_XENOMORPH_SUNDER_REGEN, seconds_per_tick)
 
 	adjust_sunder(sunder_recov)
 
-/mob/living/carbon/xenomorph/proc/handle_critical_health_updates()
+/mob/living/carbon/xenomorph/proc/handle_critical_health_updates(seconds_per_tick)
 	if(loc_weeds_type)
-		heal_wounds(XENO_RESTING_HEAL)
+		heal_wounds(XENO_RESTING_HEAL, seconds_per_tick)
 	else if(!endure) //If we're not Enduring we bleed out
-		adjust_brute_loss(XENO_CRIT_DAMAGE)
+		adjust_brute_loss(XENO_CRIT_DAMAGE * seconds_per_tick * XENO_PER_SECOND_LIFE_MOD)
 
-/mob/living/carbon/xenomorph/proc/heal_wounds(multiplier = XENO_RESTING_HEAL, scaling = FALSE)
+/mob/living/carbon/xenomorph/proc/heal_wounds(multiplier = XENO_RESTING_HEAL, scaling = FALSE, seconds_per_tick)
 	var/amount = 1 + (maxHealth * 0.0375) // 1 damage + 3.75% max health, with scaling power.
 	if(recovery_aura)
 		amount += recovery_aura * maxHealth * 0.01 // +1% max health per recovery level, up to +5%
@@ -100,19 +100,19 @@
 		if(recovery_aura)
 			regen_power = clamp(regen_power + xeno_caste.regen_ramp_amount*30,0,1) //Ignores the cooldown, and gives a 50% boost.
 		else if(regen_power < 0) // We're not supposed to regenerate yet. Start a countdown for regeneration.
-			regen_power += 2 SECONDS //Life ticks are 2 seconds.
+			regen_power += seconds_per_tick
 			return
 		else
 			regen_power = min(regen_power + xeno_caste.regen_ramp_amount*20,1)
 		amount *= regen_power
-	amount *= multiplier * GLOB.xeno_stat_multiplicator_buff
+	amount *= multiplier * GLOB.xeno_stat_multiplicator_buff * seconds_per_tick * XENO_PER_SECOND_LIFE_MOD
 
 	var/list/heal_data = list(amount)
-	SEND_SIGNAL(src, COMSIG_XENOMORPH_HEALTH_REGEN, heal_data)
-	heal_xeno_damage(heal_data[1], TRUE)
+	SEND_SIGNAL(src, COMSIG_XENOMORPH_HEALTH_REGEN, heal_data, seconds_per_tick)
+	heal_xeno_damage(src, heal_data[1], TRUE)
 	return heal_data[1]
 
-/mob/living/carbon/xenomorph/proc/handle_living_plasma_updates()
+/mob/living/carbon/xenomorph/proc/handle_living_plasma_updates(seconds_per_tick)
 	var/turf/T = loc
 	if(!istype(T)) //This means plasma doesn't update while you're in things like a vent, but since you don't have weeds in a vent or can actually take advantage of pheros, this is fine
 		return
@@ -126,7 +126,7 @@
 			QDEL_NULL(current_aura)
 			src.balloon_alert(src, "Stop emitting, no plasma")
 		else
-			use_plasma(pheromone_cost, FALSE)
+			use_plasma(pheromone_cost * seconds_per_tick * XENO_PER_SECOND_LIFE_MOD, FALSE)
 
 	if(HAS_TRAIT(src, TRAIT_NOPLASMAREGEN) || !loc_weeds_type && !(xeno_caste.caste_flags & CASTE_INNATE_PLASMA_REGEN))
 		if(current_aura) //we only need to update if we actually used plasma from pheros
@@ -140,9 +140,11 @@
 
 	plasma_gain *= loc_weeds_type ? initial(loc_weeds_type.resting_buff) : 1
 
+	plasma_gain *= seconds_per_tick * XENO_PER_SECOND_LIFE_MOD
+
 	var/list/plasma_mod = list(plasma_gain)
 
-	SEND_SIGNAL(src, COMSIG_XENOMORPH_PLASMA_REGEN, plasma_mod)
+	SEND_SIGNAL(src, COMSIG_XENOMORPH_PLASMA_REGEN, plasma_mod, seconds_per_tick)
 
 	plasma_mod[1] = clamp(plasma_mod[1], 0, xeno_caste.plasma_max * xeno_caste.plasma_regen_limit - plasma_stored)
 