tgstation/TerraGov-Marine-Corps

Fix some wierd code found using OD unimplemented (#16903)
Co-authored-by: TiviPlus <572233640+TiviPlus@users.noreply.com>

code/game/objects/machinery/adv_med.dm
@@ -168,14 +168,14 @@
 /obj/machinery/bodyscanner/ex_act(severity)
 	switch(severity)
 		if(EXPLODE_DEVASTATE)
-			for(var/atom/movable/A as mob|obj in src)
+			for(var/atom/movable/A AS in src)
 				A.loc = src.loc
 				ex_act(severity)
 			qdel(src)
 			return
 		if(EXPLODE_HEAVY)
 			if (prob(50))
-				for(var/atom/movable/A as mob|obj in src)
+				for(var/atom/movable/A AS in src)
 					A.loc = src.loc
 					ex_act(severity)
 				qdel(src)
@@ -183,7 +183,7 @@
 		if(EXPLODE_LIGHT)
 			if(!prob(75))
 				return
-			for(var/atom/movable/A as mob|obj in src)
+			for(var/atom/movable/A AS in src)
 				A.loc = src.loc
 				ex_act(severity)
 			qdel(src)

PMC-Unga-Marines/Unga-Marines

code/game/objects/machinery/adv_med.dm
@@ -165,8 +165,8 @@
 	if(!prob(severity * 0.3))
 		return
 
-	for(var/atom/movable/our_atom as mob|obj in src)
-		our_atom.loc = src.loc
+	for(var/atom/movable/our_atom AS in src)
+		our_atom.loc = loc
 		ex_act(severity)
 	qdel(src)
 