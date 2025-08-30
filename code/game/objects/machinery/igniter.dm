tgstation/TerraGov-Marine-Corps

Removes the weird spaces at the beginnings of some messages (#17226)
Co-authored-by: Lumipharon <amadeusrandom@gmail.com>
Co-authored-by: TiviPlus <57223640+TiviPlus@users.noreply.github.com>

code/game/objects/machinery/igniter.dm
@@ -71,10 +71,10 @@
 	else if(isscrewdriver(I))
 		disable = !disable
 		if(disable)
-			user.visible_message(span_warning(" [user] has disabled the [src]!"), span_warning(" You disable the connection to the [src]."))
+			user.visible_message(span_warning("[user] has disabled the [src]!"), span_warning("You disable the connection to the [src]."))
 			icon_state = "[base_state]-d"
 		else
-			user.visible_message(span_warning(" [user] has reconnected the [src]!"), span_warning(" You fix the connection to the [src]."))
+			user.visible_message(span_warning("[user] has reconnected the [src]!"), span_warning("You fix the connection to the [src]."))
 			if(powered())
 				icon_state = "[base_state]"
 			else

PMC-Unga-Marines/Unga-Marines

code/game/objects/machinery/igniter.dm
@@ -68,10 +68,10 @@
 	. = ..()
 	disable = !disable
 	if(disable)
-		user.visible_message(span_warning(" [user] has disabled the [src]!"), span_warning(" You disable the connection to the [src]."))
+		user.visible_message(span_warning("[user] has disabled the [src]!"), span_warning(" You disable the connection to the [src]."))
 		icon_state = "[base_state]-d"
 	else
-		user.visible_message(span_warning(" [user] has reconnected the [src]!"), span_warning(" You fix the connection to the [src]."))
+		user.visible_message(span_warning("[user] has reconnected the [src]!"), span_warning(" You fix the connection to the [src]."))
 		if(powered())
 			icon_state = "[base_state]"
 		else