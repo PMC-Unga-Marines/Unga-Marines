tgstation/TerraGov-Marine-Corps

Fix drop pods not centering screen text (#17246)
Co-authored-by: TiviPlus <572233640+TiviPlus@users.noreply.com>

code/__DEFINES/text.dm
@@ -31,9 +31,9 @@
 
 #define MAPTEXT_HUDMESSAGE_TITLE(text, alignment) {"<span style='font-family: \"F25 Bank\"; font-size: 12pt; line-height: 1.25; text-align: [##alignment]'><u>[##text]</u></span>"}
 
-#define MAPTEXT_HUDMESSAGE(text) {"<span style='font-family: \"F25 Bank\"; font-size: 10pt; line-height: 1; -dm-text-outline: 1px black'>[##text]</span>"}
+#define MAPTEXT_HUDMESSAGE(text, alignment) {"<span style='font-family: \"F25 Bank\"; font-size: 10pt; line-height: 1; text-align: [##alignment]; -dm-text-outline: 1px black'>[##text]</span>"}
 
-#define HUD_ANNOUNCEMENT_FORMATTING(title, text, alignment) (MAPTEXT_HUDMESSAGE_TITLE(##title, ##alignment) + "<br>" + MAPTEXT_HUDMESSAGE(##text))
+#define HUD_ANNOUNCEMENT_FORMATTING(title, text, alignment) (MAPTEXT_HUDMESSAGE_TITLE(##title, ##alignment) + "<br>" + MAPTEXT_HUDMESSAGE(##text, ##alignment))
 
 /**
  * Prepares a text to be used for maptext, using a variable size font.

PMC-Unga-Marines/Unga-Marines

code/__DEFINES/text.dm
@@ -29,9 +29,9 @@
 
 #define MAPTEXT_HUDMESSAGE_TITLE(text, alignment) {"<span style='font-family: \"MS Serif\"; font-size: 12pt; line-height: 1.25; text-align: [##alignment]'><u>[##text]</u></span>"}
 
-#define MAPTEXT_HUDMESSAGE(text) {"<span style='font-family: \"MS Serif\"; font-size: 10pt; line-height: 1; -dm-text-outline: 1px black'>[##text]</span>"}
+#define MAPTEXT_HUDMESSAGE(text, alignment) {"<span style='font-family: \"MS Serif\"; font-size: 10pt; line-height: 1; text-align: [##alignment]; -dm-text-outline: 1px black'>[##text]</span>"}
 
-#define HUD_ANNOUNCEMENT_FORMATTING(title, text, alignment) (MAPTEXT_HUDMESSAGE_TITLE(##title, ##alignment) + "<br>" + MAPTEXT_HUDMESSAGE(##text))
+#define HUD_ANNOUNCEMENT_FORMATTING(title, text, alignment) (MAPTEXT_HUDMESSAGE_TITLE(##title, ##alignment) + "<br>" + MAPTEXT_HUDMESSAGE(##text, ##alignment))
 
 /**
  * Prepares a text to be used for maptext, using a variable size font.