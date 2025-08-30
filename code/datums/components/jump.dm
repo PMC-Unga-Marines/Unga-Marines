tgstation/TerraGov-Marine-Corps

Human mob AI (#17186)

code/datums/components/jump.dm
@@ -41,7 +41,7 @@
 	RegisterSignal(parent, COMSIG_VEHICLE_GRANT_CONTROL_FLAG, PROC_REF(set_external_user))
 
 /datum/component/jump/UnregisterFromParent()
-	UnregisterSignal(parent, list(COMSIG_KB_LIVING_JUMP_UP, COMSIG_KB_LIVING_JUMP_DOWN, COMSIG_MOB_THROW, COMSIG_VEHICLE_GRANT_CONTROL_FLAG))
+	UnregisterSignal(parent, list(COMSIG_KB_LIVING_JUMP_UP, COMSIG_KB_LIVING_JUMP_DOWN, COMSIG_MOB_THROW, COMSIG_VEHICLE_GRANT_CONTROL_FLAG, COMSIG_AI_JUMP, COMSIG_LIVING_CAN_JUMP))
 	if(external_user)
 		remove_external_user()
 
@@ -58,12 +58,13 @@
 	jump_flags = _jump_flags
 	jumper_allow_pass_flags = _jumper_allow_pass_flags
 
-	UnregisterSignal(parent, list(COMSIG_KB_LIVING_JUMP_DOWN, COMSIG_KB_LIVING_JUMP_UP))
+	UnregisterSignal(parent, list(COMSIG_KB_LIVING_JUMP_DOWN, COMSIG_KB_LIVING_JUMP_UP, COMSIG_AI_JUMP, COMSIG_LIVING_CAN_JUMP))
+	RegisterSignal(parent, COMSIG_LIVING_CAN_JUMP, PROC_REF(can_jump))
 	if(jump_flags & JUMP_CHARGEABLE)
 		RegisterSignal(parent, COMSIG_KB_LIVING_JUMP_DOWN, PROC_REF(charge_jump))
-		RegisterSignal(parent, COMSIG_KB_LIVING_JUMP_UP, PROC_REF(start_jump))
+		RegisterSignals(parent, list(COMSIG_KB_LIVING_JUMP_UP, COMSIG_AI_JUMP), PROC_REF(start_jump))
 	else
-		RegisterSignal(parent, COMSIG_KB_LIVING_JUMP_DOWN, PROC_REF(start_jump))
+		RegisterSignals(parent, list(COMSIG_KB_LIVING_JUMP_DOWN, COMSIG_AI_JUMP), PROC_REF(start_jump))
 
 ///Sets an external controller, such as a vehicle driver
 /datum/component/jump/proc/set_external_user(datum/source, mob/new_user, control_flags = VEHICLE_CONTROL_DRIVE)
@@ -87,44 +88,47 @@
 	external_user = null
 
 ///Starts charging the jump
-/datum/component/jump/proc/charge_jump(mob/living/jumper)
+/datum/component/jump/proc/charge_jump(atom/movable/jumper)
 	jump_start_time = world.timeofday
 
-///handles pre-jump checks and setup of additional jump behavior.
-/datum/component/jump/proc/start_jump(mob/living/jumper)
+///Checks if you can actually jump right now
+/datum/component/jump/proc/can_jump(atom/movable/jumper)
 	SIGNAL_HANDLER
-
-	if(jumper == external_user)
-		jumper = parent
-
 	if(TIMER_COOLDOWN_CHECK(jumper, JUMP_COMPONENT_COOLDOWN))
-		return
-
+		return FALSE
 	var/mob/living/living_jumper
 	if(isliving(jumper))
 		living_jumper = jumper
-		if(jumper.buckled)
-			return
-		if(jumper.incapacitated())
-			return
+		if(living_jumper.buckled)
+			return FALSE
+		if(living_jumper.incapacitated())
+			return FALSE
+		if(stamina_cost && (living_jumper.getStaminaLoss() > -stamina_cost))
+			if(isrobot(living_jumper) || issynth(living_jumper))
+				to_chat(living_jumper, span_warning("Your leg servos do not allow you to jump!"))
+				return FALSE
+			to_chat(living_jumper, span_warning("Catch your breath!"))
+			return FALSE
+	return TRUE
 
-		if(stamina_cost && (jumper.getStaminaLoss() > -stamina_cost))
-			if(isrobot(jumper) || issynth(jumper))
-				to_chat(jumper, span_warning("Your leg servos do not allow you to jump!"))
-				return
-			to_chat(jumper, span_warning("Catch your breath!"))
-			return
+///handles pre-jump checks and setup of additional jump behavior.
+/datum/component/jump/proc/start_jump(atom/movable/jumper)
+	SIGNAL_HANDLER
+	if(jumper == external_user)
+		jumper = parent
+	if(!can_jump(jumper))
+		return
 
 	do_jump(jumper)
-	if(living_jumper)
+	if(isliving(jumper))
+		var/mob/living/living_jumper = jumper
 		living_jumper.adjustStaminaLoss(stamina_cost)
 	//Forces all who ride to jump alongside the jumper.
 	for(var/mob/buckled_mob AS in jumper.buckled_mobs)
 		do_jump(buckled_mob)
 
 ///Performs the jump
-/datum/component/jump/proc/do_jump(mob/living/jumper)
-
+/datum/component/jump/proc/do_jump(atom/movable/jumper)
 	var/effective_jump_duration = jump_duration
 	var/effective_jump_height = jump_height
 	var/effective_jumper_allow_pass_flags = jumper_allow_pass_flags
@@ -184,3 +188,17 @@
 	throw_modifiers["targetted_throw"] = FALSE
 	throw_modifiers["speed_modifier"] -= 1
 	throw_modifiers["range_modifier"] += 4
+
+///Can this be jumped over
+/atom/movable/proc/is_jumpable(mob/living/jumper)
+	if(allow_pass_flags & (PASS_LOW_STRUCTURE|PASS_TANK))
+		return TRUE
+
+/obj/structure/barricade/is_jumpable(mob/living/jumper)
+	if(is_wired)
+		return FALSE
+	return ..()
+
+///Checks if this mob can jump
+/mob/living/proc/can_jump()
+	return SEND_SIGNAL(src, COMSIG_LIVING_CAN_JUMP)

code/modules/ai/ai_behaviors/human_mobs/self_heal.dm
@@ -1,0 +1,155 @@
+/datum/ai_behavior/human
+	///Chat lines for trying to heal
+	var/list/healing_chat = list("Healing, cover me!", "Healing over here.", "Where's the damn medic?", "Medic!", "Treating wounds.", "It's just a flesh wound.", "Need a little help here!", "Cover me!.")
+	///Chat lines for retreating on low health
+	var/list/retreating_chat = list("Falling back!", "Cover me, I'm hit!", "I'm hit!", "Medic!", "Disengaging!", "Help me!", "Need a little help here!", "Tactical withdrawal.", "Repositioning.")
+
+///Reacts if the mob is below the min health threshold
+/datum/ai_behavior/human/proc/check_for_critical_health(datum/source, damage)
+	SIGNAL_HANDLER
+	COOLDOWN_START(src, ai_damage_cooldown, 5 SECONDS)
+	if(current_action == MOVING_TO_SAFETY)
+		return
+	var/mob/living/living_mob = mob_parent
+	if(!(human_ai_behavior_flags & HUMAN_AI_SELF_HEAL) || living_mob.health - damage > minimum_health * living_mob.maxHealth)
+		return
+	if(mob_parent.incapacitated() || mob_parent.lying_angle) //todo: maybe remove or change this when we add team healing
+		return
+	if(!check_hazards())
+		return
+	var/atom/next_target = get_nearest_target(mob_parent, target_distance, TARGET_HOSTILE, mob_parent.faction, need_los = TRUE)
+	if(!next_target)
+		INVOKE_ASYNC(src, PROC_REF(try_heal))
+		return
+	if(prob(50))
+		try_speak(pick(retreating_chat))
+	set_run(TRUE)
+	target_distance = 12
+	change_action(MOVING_TO_SAFETY, next_target, list(INFINITY))
+
+///Will try healing if possible
+/datum/ai_behavior/human/proc/try_heal()
+	var/mob/living/living_parent = mob_parent
+	if(living_parent.on_fire)
+		living_parent.do_resist()
+		return
+
+	if(!COOLDOWN_CHECK(src, ai_damage_cooldown))
+		return
+
+	if(prob(75))
+		try_speak(pick(healing_chat))
+	var/list/dam_list = list(
+		BRUTE = living_parent.getBruteLoss(),
+		BURN = living_parent.getFireLoss(),
+		TOX = living_parent.getToxLoss(),
+		OXY = living_parent.getOxyLoss(),
+		CLONE = living_parent.getCloneLoss(),
+		PAIN = 0,
+	)
+
+	change_action(MOB_HEALING)
+
+	if(iscarbon(mob_parent))
+		var/mob/living/carbon/carbon_parent = mob_parent
+		dam_list[PAIN] = carbon_parent.getShock_Stage() * 3 //pain is pretty important, but has low numbers and takes time to change
+
+	var/list/priority_list = sortTim(dam_list.Copy(), /proc/cmp_numeric_dsc, TRUE)
+	for(var/damtype in priority_list)
+		if(dam_list[damtype] <= 15)
+			continue
+		if(do_heal(damtype))
+			continue
+
+	if(ishuman(mob_parent))
+		var/mob/living/carbon/human/human_parent = mob_parent
+		var/list/broken_limbs = list()
+		for(var/datum/limb/limb AS in human_parent.limbs)
+			if(!(limb.limb_status & LIMB_BROKEN) || (limb.limb_status & LIMB_SPLINTED))
+				continue
+			broken_limbs += limb
+		for(var/broken_limb in broken_limbs)
+			if(!do_splint(broken_limb))
+				//send sig to call for help splinting
+				break
+
+	change_action(MOVING_TO_NODE)
+
+///Tries to heal damage of a given type
+/datum/ai_behavior/human/proc/do_heal(damtype)
+	var/obj/item/heal_item
+	var/list/med_list
+
+	switch(damtype)
+		if(BRUTE)
+			med_list = mob_inventory.brute_list
+		if(BURN)
+			med_list = mob_inventory.burn_list
+		if(TOX)
+			med_list = mob_inventory.tox_list
+		if(OXY)
+			med_list = mob_inventory.oxy_list
+		if(CLONE)
+			med_list = mob_inventory.clone_list
+		if(PAIN)
+			med_list = mob_inventory.pain_list
+
+	for(var/obj/item/stored_item AS in med_list)
+		if(!stored_item.ai_should_use(mob_parent, mob_parent))
+			continue
+		heal_item = stored_item
+		break
+
+	if(!heal_item)
+		return FALSE
+	heal_item.ai_use(mob_parent, mob_parent)
+	return TRUE
+
+///Tries to splint a limb
+/datum/ai_behavior/human/proc/do_splint(datum/limb/broken_limb)
+	var/obj/item/stack/medical/splint/splint = locate(/obj/item/stack/medical/splint) in mob_inventory.medical_list
+	if(!splint)
+		return FALSE
+	mob_parent.zone_selected = broken_limb.name //why god do we rely on the limb name, which isnt a define?
+	if(splint.attack(mob_parent, mob_parent))
+		. = TRUE
+	mob_parent.zone_selected = BODY_ZONE_CHEST
+
+//TODO: probs move these - more than just medical relevant
+
+///Checks if the AI can or should use this
+/obj/item/proc/ai_should_use(mob/living/target, mob/living/user)
+	return TRUE
+
+///AI uses this item in some manner
+/obj/item/proc/ai_use(mob/living/target, mob/living/user)
+	return FALSE
+
+/obj/item/stack/medical/ai_use(mob/living/target, mob/living/user)
+	attack(target, user) //attack chain is a bit funny between different medical items like reagent containers below
+
+/obj/item/stack/medical/heal_pack/ai_should_use(mob/living/target, mob/living/user)
+	if(!ishuman(target))
+		return FALSE
+	var/mob/living/carbon/human/human_mob = target
+	for(var/limb AS in human_mob.limbs)
+		if(can_heal_limb(limb))
+			return TRUE
+	return FALSE
+
+/obj/item/reagent_containers/ai_should_use(mob/living/target, mob/living/user)
+	for(var/datum/reagent/reagent AS in reagents.reagent_list)
+		if(reagent.volume + target.reagents.get_reagent_amount(reagent.type) > reagent.overdose_threshold)
+			return FALSE
+	return TRUE
+
+/obj/item/reagent_containers/ai_use(mob/living/target, mob/living/user)
+	afterattack(target, user, TRUE)
+
+/obj/item/reagent_containers/hypospray/ai_should_use(mob/living/target, mob/living/user)
+	if(!length(reagents.reagent_list)) //todo: discard if empty
+		return FALSE
+	for(var/datum/reagent/reagent AS in reagents.reagent_list)
+		if((reagent.volume / reagents.total_volume * amount_per_transfer_from_this) + target.reagents.get_reagent_amount(reagent.type) > reagent.overdose_threshold)
+			return FALSE
+	return TRUE

PMC-Unga-Marines/Unga-Marines

code/datums/components/jump.dm
@@ -37,7 +37,7 @@
 	set_vars(_jump_duration, _jump_cooldown, _stamina_cost, _jump_height, _jump_sound, _jump_flags, _jumper_allow_pass_flags)
 
 /datum/component/jump/UnregisterFromParent()
-	UnregisterSignal(parent, list(COMSIG_KB_LIVING_JUMP_DOWN, COMSIG_MOB_THROW))
+	UnregisterSignal(parent, list(COMSIG_KB_LIVING_JUMP_DOWN, COMSIG_KB_LIVING_JUMP_UP, COMSIG_MOB_THROW, COMSIG_AI_JUMP, COMSIG_LIVING_CAN_JUMP))
 
 /datum/component/jump/InheritComponent(datum/component/new_component, original_component, _jump_duration, _jump_cooldown, _stamina_cost, _jump_height, _jump_sound, _jump_flags, _jumper_allow_pass_flags)
 	set_vars(_jump_duration, _jump_cooldown, _stamina_cost, _jump_height, _jump_sound, _jump_flags, _jumper_allow_pass_flags)
@@ -52,42 +52,53 @@
 	jump_flags = _jump_flags
 	jumper_allow_pass_flags = _jumper_allow_pass_flags
 
-	UnregisterSignal(parent, list(COMSIG_KB_LIVING_JUMP_DOWN))
+	UnregisterSignal(parent, list(COMSIG_KB_LIVING_JUMP_DOWN, COMSIG_KB_LIVING_JUMP_UP, COMSIG_AI_JUMP, COMSIG_LIVING_CAN_JUMP))
+	RegisterSignal(parent, COMSIG_LIVING_CAN_JUMP, PROC_REF(can_jump))
 	if(jump_flags & JUMP_CHARGEABLE)
 		RegisterSignal(parent, COMSIG_KB_LIVING_JUMP_DOWN, PROC_REF(charge_jump))
-		RegisterSignal(parent, COMSIG_KB_LIVING_JUMP_UP, PROC_REF(start_jump))
+		RegisterSignals(parent, list(COMSIG_KB_LIVING_JUMP_UP, COMSIG_AI_JUMP), PROC_REF(start_jump))
 	else
-		RegisterSignal(parent, COMSIG_KB_LIVING_JUMP_DOWN, PROC_REF(start_jump))
+		RegisterSignals(parent, list(COMSIG_KB_LIVING_JUMP_DOWN, COMSIG_AI_JUMP), PROC_REF(start_jump))
 
 ///Starts charging the jump
-/datum/component/jump/proc/charge_jump(mob/living/jumper)
+/datum/component/jump/proc/charge_jump(atom/movable/jumper)
 	jump_start_time = world.timeofday
 
-///handles pre-jump checks and setup of additional jump behavior.
-/datum/component/jump/proc/start_jump(mob/living/jumper)
+///Checks if you can actually jump right now
+/datum/component/jump/proc/can_jump(atom/movable/jumper)
 	SIGNAL_HANDLER
-
 	if(TIMER_COOLDOWN_CHECK(jumper, JUMP_COMPONENT_COOLDOWN))
-		return
+		return FALSE
+	var/mob/living/living_jumper
+	if(isliving(jumper))
+		living_jumper = jumper
+		if(living_jumper.buckled)
+			return FALSE
+		if(living_jumper.incapacitated())
+			return FALSE
+		if(stamina_cost && (living_jumper.get_stamina_loss() > -stamina_cost))
+			to_chat(living_jumper, span_warning("Catch your breath!"))
+			return FALSE
+	return TRUE
 
-	if(jumper.buckled)
-		return
-	if(jumper.incapacitated())
-		return
-
-	if(stamina_cost && (jumper.get_stamina_loss() > -stamina_cost))
-		to_chat(jumper, span_warning("Catch your breath!"))
+///handles pre-jump checks and setup of additional jump behavior.
+/datum/component/jump/proc/start_jump(atom/movable/jumper)
+	SIGNAL_HANDLER
+	if(jumper == external_user)
+		jumper = parent
+	if(!can_jump(jumper))
 		return
 
 	do_jump(jumper)
-	jumper.adjust_stamina_loss(stamina_cost)
+	if(isliving(jumper))
+		var/mob/living/living_jumper = jumper
+		living_jumper.adjust_stamina_loss(stamina_cost)
 	//Forces all who ride to jump alongside the jumper.
 	for(var/mob/buckled_mob AS in jumper.buckled_mobs)
 		do_jump(buckled_mob)
 
 ///Performs the jump
-/datum/component/jump/proc/do_jump(mob/living/jumper)
-
+/datum/component/jump/proc/do_jump(atom/movable/jumper)
 	var/effective_jump_duration = jump_duration
 	var/effective_jump_height = jump_height
 	var/effective_jumper_allow_pass_flags = jumper_allow_pass_flags
@@ -146,3 +157,17 @@
 	throw_modifiers["targetted_throw"] = FALSE
 	throw_modifiers["speed_modifier"] -= 1
 	throw_modifiers["range_modifier"] += 4
+
+///Can this be jumped over
+/atom/movable/proc/is_jumpable(mob/living/jumper)
+	if(allow_pass_flags & (PASS_LOW_STRUCTURE|PASS_TANK))
+		return TRUE
+
+/obj/structure/barricade/is_jumpable(mob/living/jumper)
+	if(is_wired)
+		return FALSE
+	return ..()
+
+///Checks if this mob can jump
+/mob/living/proc/can_jump()
+	return SEND_SIGNAL(src, COMSIG_LIVING_CAN_JUMP)

code/modules/ai/ai_behaviors/human_mobs/self_heal.dm
@@ -1,0 +1,155 @@
+/datum/ai_behavior/human
+	///Chat lines for trying to heal
+	var/list/healing_chat = list("Healing, cover me!", "Healing over here.", "Where's the damn medic?", "Medic!", "Treating wounds.", "It's just a flesh wound.", "Need a little help here!", "Cover me!.")
+	///Chat lines for retreating on low health
+	var/list/retreating_chat = list("Falling back!", "Cover me, I'm hit!", "I'm hit!", "Medic!", "Disengaging!", "Help me!", "Need a little help here!", "Tactical withdrawal.", "Repositioning.")
+
+///Reacts if the mob is below the min health threshold
+/datum/ai_behavior/human/proc/check_for_critical_health(datum/source, damage)
+	SIGNAL_HANDLER
+	COOLDOWN_START(src, ai_damage_cooldown, 5 SECONDS)
+	if(current_action == MOVING_TO_SAFETY)
+		return
+	var/mob/living/living_mob = mob_parent
+	if(!(human_ai_behavior_flags & HUMAN_AI_SELF_HEAL) || living_mob.health - damage > minimum_health * living_mob.maxHealth)
+		return
+	if(mob_parent.incapacitated() || mob_parent.lying_angle) //todo: maybe remove or change this when we add team healing
+		return
+	if(!check_hazards())
+		return
+	var/atom/next_target = get_nearest_target(mob_parent, target_distance, TARGET_HOSTILE, mob_parent.faction, need_los = TRUE)
+	if(!next_target)
+		INVOKE_ASYNC(src, PROC_REF(try_heal))
+		return
+	if(prob(50))
+		try_speak(pick(retreating_chat))
+	set_run(TRUE)
+	target_distance = 12
+	change_action(MOVING_TO_SAFETY, next_target, list(INFINITY))
+
+///Will try healing if possible
+/datum/ai_behavior/human/proc/try_heal()
+	var/mob/living/living_parent = mob_parent
+	if(living_parent.on_fire)
+		living_parent.do_resist()
+		return
+
+	if(!COOLDOWN_CHECK(src, ai_damage_cooldown))
+		return
+
+	if(prob(75))
+		try_speak(pick(healing_chat))
+	var/list/dam_list = list(
+		BRUTE = living_parent.get_brute_loss(),
+		BURN = living_parent.get_fire_loss(),
+		TOX = living_parent.get_tox_loss(),
+		OXY = living_parent.get_oxy_loss(),
+		CLONE = living_parent.get_clone_loss(),
+		PAIN = 0,
+	)
+
+	change_action(MOB_HEALING)
+
+	if(iscarbon(mob_parent))
+		var/mob/living/carbon/carbon_parent = mob_parent
+		dam_list[PAIN] = carbon_parent.getShock_Stage() * 3 //pain is pretty important, but has low numbers and takes time to change
+
+	var/list/priority_list = sortTim(dam_list.Copy(), /proc/cmp_numeric_dsc, TRUE)
+	for(var/damtype in priority_list)
+		if(dam_list[damtype] <= 15)
+			continue
+		if(do_heal(damtype))
+			continue
+
+	if(ishuman(mob_parent))
+		var/mob/living/carbon/human/human_parent = mob_parent
+		var/list/broken_limbs = list()
+		for(var/datum/limb/limb AS in human_parent.limbs)
+			if(!(limb.limb_status & LIMB_BROKEN) || (limb.limb_status & LIMB_SPLINTED))
+				continue
+			broken_limbs += limb
+		for(var/broken_limb in broken_limbs)
+			if(!do_splint(broken_limb))
+				//send sig to call for help splinting
+				break
+
+	change_action(MOVING_TO_NODE)
+
+///Tries to heal damage of a given type
+/datum/ai_behavior/human/proc/do_heal(damtype)
+	var/obj/item/heal_item
+	var/list/med_list
+
+	switch(damtype)
+		if(BRUTE)
+			med_list = mob_inventory.brute_list
+		if(BURN)
+			med_list = mob_inventory.burn_list
+		if(TOX)
+			med_list = mob_inventory.tox_list
+		if(OXY)
+			med_list = mob_inventory.oxy_list
+		if(CLONE)
+			med_list = mob_inventory.clone_list
+		if(PAIN)
+			med_list = mob_inventory.pain_list
+
+	for(var/obj/item/stored_item AS in med_list)
+		if(!stored_item.ai_should_use(mob_parent, mob_parent))
+			continue
+		heal_item = stored_item
+		break
+
+	if(!heal_item)
+		return FALSE
+	heal_item.ai_use(mob_parent, mob_parent)
+	return TRUE
+
+///Tries to splint a limb
+/datum/ai_behavior/human/proc/do_splint(datum/limb/broken_limb)
+	var/obj/item/stack/medical/splint/splint = locate(/obj/item/stack/medical/splint) in mob_inventory.medical_list
+	if(!splint)
+		return FALSE
+	mob_parent.zone_selected = broken_limb.name //why god do we rely on the limb name, which isnt a define?
+	if(splint.attack(mob_parent, mob_parent))
+		. = TRUE
+	mob_parent.zone_selected = BODY_ZONE_CHEST
+
+//TODO: probs move these - more than just medical relevant
+
+///Checks if the AI can or should use this
+/obj/item/proc/ai_should_use(mob/living/target, mob/living/user)
+	return TRUE
+
+///AI uses this item in some manner
+/obj/item/proc/ai_use(mob/living/target, mob/living/user)
+	return FALSE
+
+/obj/item/stack/medical/ai_use(mob/living/target, mob/living/user)
+	attack(target, user) //attack chain is a bit funny between different medical items like reagent containers below
+
+/obj/item/stack/medical/heal_pack/ai_should_use(mob/living/target, mob/living/user)
+	if(!ishuman(target))
+		return FALSE
+	var/mob/living/carbon/human/human_mob = target
+	for(var/limb AS in human_mob.limbs)
+		if(can_heal_limb(limb))
+			return TRUE
+	return FALSE
+
+/obj/item/reagent_containers/ai_should_use(mob/living/target, mob/living/user)
+	for(var/datum/reagent/reagent AS in reagents.reagent_list)
+		if(reagent.volume + target.reagents.get_reagent_amount(reagent.type) > reagent.overdose_threshold)
+			return FALSE
+	return TRUE
+
+/obj/item/reagent_containers/ai_use(mob/living/target, mob/living/user)
+	afterattack(target, user, TRUE)
+
+/obj/item/reagent_containers/hypospray/ai_should_use(mob/living/target, mob/living/user)
+	if(!length(reagents.reagent_list)) //todo: discard if empty
+		return FALSE
+	for(var/datum/reagent/reagent AS in reagents.reagent_list)
+		if((reagent.volume / reagents.total_volume * amount_per_transfer_from_this) + target.reagents.get_reagent_amount(reagent.type) > reagent.overdose_threshold)
+			return FALSE
+	return TRUE