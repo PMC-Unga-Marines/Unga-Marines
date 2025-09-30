536764a3d99b5a1787d73c1c4d224e63acbd7094
Helg2 <skubi.ole@gmail.com>
2025-09-30 16:38:24 +0300

    eh

--- tools/ci/download_byond.sh
+++ tools/ci/download_byond.sh
@@ -2,4 +2,4 @@
 set -e
 source dependencies.sh
 echo "Downloading BYOND version $BYOND_MAJOR.$BYOND_MINOR"
-curl -H "User-Agent: PMC-Unga-Marines/1.0 CI Script" "http://www.byond.com/download/build/515/515.1647_byond.zip" -o C:/byond.zip
+curl "http://www.byond.com/download/build/$BYOND_MAJOR/$BYOND_MAJOR.${BYOND_MINOR}_byond.zip" -o C:/byond.zip
