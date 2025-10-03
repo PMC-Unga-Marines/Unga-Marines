5c64eccac051f4d3f6ea983ea374f5150bce1088
Helg2 <skubi.ole@gmail.com>
2025-10-03 18:10:20 +0300

    Update JobPreferences.tsx

--- tgui/packages/tgui/interfaces/PlayerPreferences/JobPreferences.tsx
+++ tgui/packages/tgui/interfaces/PlayerPreferences/JobPreferences.tsx
@@ -51,10 +51,7 @@ export const JobPreferences = (props) => {
     'Squad Smartgunner',
     'Squad Leader',
   ];
-  const flavourJobs = [
-    'Corporate Liaison',
-    'Survivor',
-  ];
+  const flavourJobs = ['Corporate Liaison', 'Survivor'];
 
   const JobList = ({ name, jobs }) => (
     <Section title={name}>
